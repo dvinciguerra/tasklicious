8e34739ef567777db1e03b91de2215c58002aa12
Daniel Vinciguerra <dan.vinciguerra@gmail.com>
2014-02-04 00:24:45 -0200

    start create project api

---
+++ lib/Tasklicious/API/Project.pm
@@ -0,0 +1,28 @@
+package Tasklicious::API::Project;
+use Mojo::Base 'Tasklicious::API::Base';
+
+use Mojo::JSON;
+use Tasklicious::Model;
+
+sub get_list {
+    my $self = shift;
+}
+
+sub get_load {
+    my $self = shift;
+} 
+
+sub edit {
+    my $self = shift;
+}
+
+sub create {
+    my $self = shift;
+}
+
+sub delete {
+    my $self = shift;
+}
+
+
+1;
